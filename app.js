74c8a13897d71465e241f46f9e695c93473d510f
Anatoli <anatoli.railean@gmail.com>
2022-11-13 16:56:10 -0800

    Added logic to check a connection

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Function to check if a stream is up
+ */
+const getData = () => {
+    fetch(process.env.STREAM_URL)
+      .then(response => {
+        response.json();
+        console.log(`Stream is up: ${new Date().toLocaleDateString()}`)
+        .catch(err => {
+            console.error(err);
+            console.error(`Stream is down!`);
+        });
+    })
+  }
+
+const interval = setInterval(() => {
+    getData();
+  }, 1000);
